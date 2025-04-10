27fe5a36861f7860f482630f341c1ef61bba4297
Shivam829l <sk9400749@gmail.com>
2025-04-10 08:02:14 +0000

    var

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,13 @@
+const accountId=14445553 /* it can be changed or redeclare */
+let accountEmail= "litesh@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+
+// accountId=2 //not allwed
+console.log(accountId);
+
+accounEmail= "sk94400gamail"
+accountCity="21132"
+accountPassword="89798789"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
