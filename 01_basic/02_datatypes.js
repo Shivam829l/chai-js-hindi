8a6a005b96cfb203d035b871deead561ea2b852c
Shivam829l <sk9400749@gmail.com>
2025-04-10 08:02:01 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"// treat all JS code as newer version
+ 
+// alert(3+3) // we are using node js , not a browser
+
+let name="hitesh"
+let age= 27
+let isLoggedin= true
+
+//number => 2 to power 53
+// bigInt
+// string=> ""
+// boolean => true/false
+//null => standalone value
+// undefined => 
+// symbol => uniqueness
+
+
+// object
+
+console.log(typeof "hitesh")
+console.log(typeof age)
+
